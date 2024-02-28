07df4768709a32d32fb0342136d6136903e7ef57
w-ky <45529935+w-ky@users.noreply.github.com>
2024-02-28 16:56:14 +0800

    Create index.vue

---
+++ src/components/table-render/index.vue
@@ -0,0 +1,173 @@
+<script>
+export default {
+  name: 'Table',
+  inject: ['getCurLangVal'],
+  props: [
+    tableData: {
+      type: Array,
+      default: () => ([])
+    },
+    tableColumns: {
+      type: Array,
+      default: () => ([])
+    },
+    emptyText: {
+      type: String,
+      default: '暂无数据'
+    },
+    emptyBtnText: {
+      type: String,
+      default: null
+    },
+    emptyBtnIcon: {
+      type: String,
+      default: null
+    },
+    expandKey: {
+      type: String,
+      default: null
+    },
+    page: Number,
+    limit: Number,
+    total: Number
+  ],
+  methods: {
+    isCustomColumn (column) {
+      return (
+        column.type && !['text', 'index', 'selection'].includes(column.type)
+      )
+    },
+    getColumnType (column) {
+      if (
+        ['expand', 'index', 'selection'].includes(column.type) ||
+        column.prop !== this.expandKey
+      ) {
+        return column.type
+      }
+      return undefined
+    },
+    getAlign (column) {
+      if (column.align) return column.align
+        return column.type === 'index' ? 'center' : undefined
+    },
+        
+    getIndex (row, column, cellValue, index) {
+      return index + 1
+    },
+        
+    getFormatter (column) {
+      if (column.formatter) return column.formatter
+      if (column.type === 'index') return this.getIndex
+      return (row, column, cellValue, index) => {
+        return cellValue || cellValue === 0 ? cellValue : '-'
+      }
+    },
+        
+    renderTableItem (column) {
+      const name = column.headerKey || 'header' + column.prop
+      const scopedSlots = {}
+      if (column.headerType === 'slot' && this.$scopedSlots[name]) {
+        scopedSlots.header = () => this.$scopedSlots[name]?.()
+      } 
+      if (this.isCustomColumn(column)) {
+        scopedSlots.default = (scope) => {
+          if (column.type === 'lang') {
+            return (
+              <span>{this.getCurLangVal(scope.row, column.prop) || '-'}</span>
+            )
+          }
+          return this.$scopedSlots[column.prop]?.(scope)
+        }
+      }
+      if (Array.isArray(column.child)) {
+        return (
+          <el-table-column
+            scopedSlots={scopedSlots}
+            {...{ attrs: column }}
+            type={this.getColumnType(column)}
+            align={this.getAlign(column)}
+            formatter={this.getFormatter(column)}
+          >
+            {column.child.map(item => this.renderTableItem(item))}
+          </el-table-column>
+        )
+      }
+      return (
+        <el-table-column
+          scopedSlots={scopedSlots}
+          {...{ attrs: column }}
+          type={this.getColumnType(column)}
+          align={this.getAlign(column)}
+          formatter={this.getFormatter(column)}
+        />
+      )   
+    },
+
+    renderTable () {
+      const img = require('xxxxx.png')
+      const slots = {
+        empty: () => {
+          if (this.$slots.empty) return this.$slots.empty()
+          return (
+            <div class="table-empty">
+              <img src={img} />
+              <p>{this.emptyText}</p>
+              {
+                this.emptyBtnText ?
+                (
+                  <el-button
+                    icon={this.emptyBtnIcon}
+                    onClick={this.emptyBtnClick}
+                    class="empty-btn"
+                    type="primary"
+                  >
+                  </el-button>
+                ) : undefined
+              }
+            </div>
+          )
+        }
+      }
+      if (this.$scopedSlots.append) {
+        slots.append = () => this.$scopedSlots.append()
+      }
+      return (
+        <div class="table">
+          <el-table
+            scopedSlots={slots}
+            data={this.tableData}
+            tooltip-effect="light"
+            ref="table"
+            {...{ on: this.$listeners, attrs: this.$attrs }}
+          >
+            <div slot="append"></div>
+            {this.tableColumns.map(column => this.renderTableItem(column))}
+          </el-table>
+          {
+            this.page && this.limit && this.total ? 
+            (
+              <el-pagination
+                page={this.page}
+                limit={this.limit}
+                total={this.total}
+                {...{ on: this.$listeners }}
+              >
+              </el-pagination>
+            ) : undefined
+          }
+        </div>
+      )
+    },
+    emptyClick () {
+      this.$emit('empty-btn-click')
+    },
+    doLayout () {
+      if (!this.$refs.table) return
+      return this.$refs.table.doLayout
+    }
+  },
+  render () {
+    return this.renderTable()
+  }
+}
+</script>
